fc7c6bf1cee49cb6a27e4456472fa5966c6672dd
K Ravi Kumar <kravi@geschaftindia.com>
2025-07-21 10:38:29 +0000

    21072025

--- webapp/controller/mysecondview1.controller.js
+++ webapp/controller/mysecondview1.controller.js
@@ -2,9 +2,11 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ], (Controller) => {
     "use strict";
-
     return Controller.extend("com.test.ravifirstsapui5.controller.mysecondview1", {
         onInit() {
+        },
+        clickb2: function () {
+          history.go(-1);
         }
     });
 });
---
+++ webapp/controller/simpleform.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.test.ravifirstsapui5.controller.simpleform", {
+        onInit() {
+        }
+        // clickb1: function () {
+        //   this.getOwnerComponent().getRouter().navTo("Routemysecondview1");
+        // },
+        // onSubmit: function (){
+        //     var name = this.getView().byId("idIpName").getValue();
+        //     var msg = name + " first screen";
+        //     // alert(msg);
+        //     this.getView().byId("idTextFirst").setText(msg);
+        //     this.getView().byId("idBtnsubmit").setType("Reject");
+        //     this.getView().byId("idTextFirst").setTextAlign("Left");
+        //     this.getView().byId("idLb1").setRequired(false);
+        //     this.getView().byId("idIpName").setEnabled(false);
+        // }
+    });
+});
